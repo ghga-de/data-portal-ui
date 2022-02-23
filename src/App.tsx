579c8f7f7058b687e30eb34b24ee0471ed38d7fd
alamgirmani <alamgir.mani@gmail.com>
2022-02-23 13:11:52 +0000

    Added basic routes and pageNotFound

--- src/App.tsx
+++ src/App.tsx
@@ -1,16 +1,21 @@
 import Header from './components/header/header'
 import Footer from './components/footer/footer'
 import Browse from './components/browse/browse'
+import PageNotFound from './components/pageNotFound/pageNotFound'
 import './App.scss'
+import { BrowserRouter, Switch, Route } from 'react-router-dom'
 
 
 function App() {
   return (
-    <div>
+    <BrowserRouter>
       <Header />
-      <Browse />
+      <Switch>
+        <Route path="/" component={Browse} exact />
+        <Route component={PageNotFound} />
+      </Switch>
       <Footer />
-    </div>
+    </BrowserRouter>
   );
 }
 
