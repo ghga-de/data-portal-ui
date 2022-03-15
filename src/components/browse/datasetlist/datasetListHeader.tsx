4ff73b9487ab9cac8a83de75ac9fd70c20310ffd
alamgirmani <alamgir.mani@gmail.com>
2022-03-15 13:56:45 +0000

    Added badges to the DatalistHeader

--- src/components/browse/datasetlist/datasetListHeader.tsx
+++ src/components/browse/datasetlist/datasetListHeader.tsx
@@ -4,13 +4,31 @@ import { Arrow90degLeft } from 'react-bootstrap-icons'
 
 interface dataSetListHeaderProps {
   dsCount: number;
+  searchParams: any;
 };
 
 const DatasetListHeader = (props: dataSetListHeaderProps) => {
+  const getFilterParamsList = () => {
+    let filterParamsList = []
+    if(props.searchParams.get("f") !== undefined && props.searchParams.get("f") !== null){
+      let searchParamsList = props.searchParams.get("f").split(';')
+      for (var item of searchParamsList){
+        filterParamsList.push(item.replace(':', ' | '))
+      }
+    }
+    return filterParamsList
+  }
+  
+  
   return (
     <Row className='mt-3 pe-4'>
-      <Col lg md sm xl xs xxl="9">
+      <Col lg md sm xl xs xxl="3">
         <Badge className='p-2 bg-secondary'>Datasets Found: {props.dsCount}</Badge>
+      </Col>
+      <Col lg md sm xl xs xxl="6">
+        {getFilterParamsList().map((item) => 
+          <Badge className='p-2 bg-secondary'>{item}</Badge>
+        )}
       </Col>
       <Col className='text-end'>
         <a href="/browse" className='text-reset fs-7 mt-2'>Back to all datasets&nbsp;<Arrow90degLeft/></a>
