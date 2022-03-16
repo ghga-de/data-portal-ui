b1de0b4cc0ab7e0c885df1bb2a9977c969504aff
alamgirmani <alamgir.mani@gmail.com>
2022-03-16 08:18:18 +0000

    updated CSS for badges

--- src/components/browse/datasetlist/datasetListHeader.tsx
+++ src/components/browse/datasetlist/datasetListHeader.tsx
@@ -10,28 +10,28 @@ interface dataSetListHeaderProps {
 const DatasetListHeader = (props: dataSetListHeaderProps) => {
   const getFilterParamsList = () => {
     let filterParamsList = []
-    if(props.searchParams.get("f") !== undefined && props.searchParams.get("f") !== null){
+    if (props.searchParams.get("f") !== undefined && props.searchParams.get("f") !== null) {
       let searchParamsList = props.searchParams.get("f").split(';')
-      for (var item of searchParamsList){
+      for (var item of searchParamsList) {
         filterParamsList.push(item.replace(':', ' | '))
       }
     }
     return filterParamsList
   }
-  
-  
+
+
   return (
     <Row className='mt-3 pe-4'>
-      <Col lg md sm xl xs xxl="3">
+      <Col lg md sm xl xs xxl="2">
         <Badge className='p-2 bg-secondary'>Datasets Found: {props.dsCount}</Badge>
       </Col>
       <Col lg md sm xl xs xxl="6">
-        {getFilterParamsList().map((item) => 
-          <Badge key={item} className='p-2 bg-secondary'>{item}</Badge>
+        {getFilterParamsList().map((item) =>
+          <Badge key={item} className='p-2 bg-primary me-2'>{item}</Badge>
         )}
       </Col>
       <Col className='text-end'>
-        <a href="/browse" className='text-reset fs-7 mt-2'>Back to all datasets&nbsp;<Arrow90degLeft/></a>
+        <a href="/browse" className='text-reset fs-7 mt-2'>Back to all datasets&nbsp;<Arrow90degLeft /></a>
       </Col>
     </Row>
   )
