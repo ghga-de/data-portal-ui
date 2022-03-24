3f4a93c24776b51d51fa78c89e56298f9eabe2b7
alamgirmani <57624686+alamgirmani@users.noreply.github.com>
2022-03-24 10:43:24 +0100

    Added popup for request access button (#36)
    - Added Modal component for Popup
    - Added popup message
    - closes GDEV-664

--- src/components/browse/datasetlist/datasetheader/datasetdetails/datasetDetails.tsx
+++ src/components/browse/datasetlist/datasetheader/datasetdetails/datasetDetails.tsx
@@ -1,5 +1,5 @@
-import React from "react";
-import { Row, Col, Button } from "react-bootstrap";
+import React, { useState } from "react";
+import { Row, Col, Button, Modal } from "react-bootstrap";
 import { datasetEmbeddedModel, hitModel } from "../../../../../models/dataset";
 import DatasetExperiments from "./datasetExperiments";
 import DatasetFiles from "./datasetFiles";
@@ -12,9 +12,13 @@ interface dataSetDetailsProps {
 }
 
 const DatasetDetails = (props: dataSetDetailsProps) => {
+  const mailId: string = "dac-ghga@ghga.de"
+  const [show, setShow] = useState(false);
+
+  const handleClose = () => setShow(false);
+  const handleOpen = () => setShow(true)
 
   const requestAccess = (datasetId: string, topic: string) => {
-    const mailId: string = "dac-ghga@ghga.de"
     const subject: string = "Request access for dataset " + datasetId
     const body: string = `Hello DAC team,%0D%0A%0D%0A` +
       `Since I am interested in the topic ${topic}, I would like to request access to the Dataset ${datasetId}.%0D%0A%0D%0A` +
@@ -40,8 +44,36 @@ const DatasetDetails = (props: dataSetDetailsProps) => {
             </p>
           </Col>
           <Col lg md sm xl xs xxl="1" className="text-end px-0">
-            <Button className="fs-8 w-100" onClick={() => requestAccess(props.hit.content.accession, props.hit.content.title)}>Request Access</Button>
+            {props.details !== null && props.details !== undefined ?
+              <Button className="fs-8 w-100"
+                onClick={() => handleOpen()}>Request Access</Button> :
+              <Button className="fs-8 w-100" disabled
+                onClick={() => handleOpen()}>Request Access</Button>
+            }
           </Col>
+          <Modal size="lg" centered
+            show={show} onHide={handleClose}>
+            <Modal.Header closeButton>
+              <Modal.Title>Request access for dataset {props.hit.content.accession}</Modal.Title>
+            </Modal.Header>
+
+            <Modal.Body>
+              <p>Please copy the following message and send us a mail to {mailId}. If configured, the
+                message opens in your email client. Please provide any additional details if needed.
+              </p>
+              <hr />
+              <p>Hello DAC team,
+                <br /><br />
+                Since I am interested in the topic {props.hit.content.title}, I would like to request access to the Dataset {props.hit.content.accession}.
+                Kindly grant the access for the requested dataset.
+                <br /><br />
+                Thank you
+              </p>
+            </Modal.Body>
+            <Modal.Footer>
+              <Button onClick={() => requestAccess(props.hit.content.accession, props.hit.content.title)}>Open Mail Client</Button>
+            </Modal.Footer>
+          </Modal>
         </Row>
         <p className="fs-8">
           <span className="fw-bold">Description:&nbsp;</span>
@@ -49,20 +81,22 @@ const DatasetDetails = (props: dataSetDetailsProps) => {
         </p>
       </Row>
       <hr />
-      {props.details !== null && props.details !== undefined ? (
-        <div>
-          <Row className="my-4 pt-3 fs-8">
-            <DatasetStudies studiesList={props.details.has_study} />
-            <DatasetFiles filesList={props.details.has_file} />
-          </Row>
-          <Row className="pb-4 pt-2 fs-8">
-            <DatasetSamples samplesList={props.details.has_sample} />
-            <DatasetExperiments experimentsList={props.details.has_experiment} />
-          </Row>
-        </div>
-      ) : (
-        <div>Dataset details loading, please wait.</div>
-      )}
+      {
+        props.details !== null && props.details !== undefined ? (
+          <div>
+            <Row className="my-4 pt-3 fs-8">
+              <DatasetStudies studiesList={props.details.has_study} />
+              <DatasetFiles filesList={props.details.has_file} />
+            </Row>
+            <Row className="pb-4 pt-2 fs-8">
+              <DatasetSamples samplesList={props.details.has_sample} />
+              <DatasetExperiments experimentsList={props.details.has_experiment} />
+            </Row>
+          </div>
+        ) : (
+          <div>Dataset details loading, please wait.</div>
+        )
+      }
     </div >
   );
 };
