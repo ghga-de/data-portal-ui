43eac4f794c98e02b58faf2f56668d2dfe340c10
Jordy <31940280+ac-jorellanaf@users.noreply.github.com>
2022-04-28 16:26:56 +0200

    Added Wireframe-based changes
    * Brought UI more in line with wireframes.
    * Closes GDEV-803

--- src/components/browse/sidebar/filter.tsx
+++ src/components/browse/sidebar/filter.tsx
@@ -33,10 +33,10 @@ const Filter = (props: filterProps) => {
   };
 
   return (
-    <Container className="col-11 mb-3 fs-7 mx-0 w-100">
+    <Container className="col-11 mb-3 fs-7 mx-0 w-100 p-0">
       <Form>
-        <Form.Label className="mt-0 mb-4 text-capitalize fs-5"><strong>{props.facet.name}</strong></Form.Label>
-        <div className="border rounded border-light px-2 pt-3 pb-4">
+        <Form.Label className="mt-0 mb-3 text-capitalize fs-5"><strong>{props.facet.name}</strong></Form.Label>
+        <div className="border rounded border-light px-2 pt-3 pb-4 mb-3">
         {props.facet.options
           .sort((a, b) => (b.option < a.option ? 1 : -1))
           .map((option) => {
@@ -54,12 +54,12 @@ const Filter = (props: filterProps) => {
                   />
                   <Form.Label className="p-0 m-0 w-100" htmlFor={key}  style={{cursor: "pointer"}}>
                     <Row>
-                      <Col lg={9} md={9} sm={9} xl={9} xs={9} xxl={9}>
+                      <Col lg={10} md={10} sm={10} xl={10} xs={10} xxl={10}>
                         <p className="ps-2 my-0 text-capitalize">{option.option}</p>
                       </Col>
-                      <Col lg={3} md={3} sm={3} xl={3} xs={3} xxl={3} className="h-100">
+                      <Col lg={2} md={2} sm={2} xl={2} xs={2} xxl={2} className="h-100 ps-0 pe-3">
                         <Form.Label className="p-0 m-0 w-100" htmlFor={key}>
-                          <Badge className="w-100 user-select-none"  style={{cursor: "pointer"}}>{option.count}</Badge>
+                          <Badge className="w-100 user-select-none px-0"  style={{cursor: "pointer"}}>{option.count}</Badge>
                         </Form.Label>
                       </Col>
                     </Row>
