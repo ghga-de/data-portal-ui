4cd1bd3d189774bb07c09414f613c05f173845c1
ac-jorellanaf <31940280+ac-jorellanaf@users.noreply.github.com>
2022-04-28 14:30:22 +0000

    Added interactivity to navbar

--- src/components/header/headerNavbar.tsx
+++ src/components/header/headerNavbar.tsx
@@ -1,24 +1,24 @@
 import React from "react";
 import logo from "../../assets/data-portal.png";
-import { Navbar, Container, Nav } from "react-bootstrap";
+import { Navbar, Container, Nav, Button } from "react-bootstrap";
 
 const HeaderNavbar = () => {
   return (
-    <Navbar collapseOnSelect expand="lg" bg="primary" variant="dark">
+    <Navbar collapseOnSelect expand="lg" bg="primary" variant="dark" className="p-0">
       <Container>
         <Navbar.Brand className="p-0" >
-          <a href="/">
+          <Button href="/" className="p-1 m-0">
             <img src={logo} alt="GHGA logo" height="50px"/>
-          </a>
+          </Button>
         </Navbar.Brand>
         <Navbar.Toggle aria-controls="basic-navbar-nav" className="border-2 text-white" />
         <Navbar.Collapse id="responsive-navbar-nav">
           <Nav className="justify-content-center flex-fill" style={{height: '36px'}}>
-            <Nav.Link href="/" className="mx-2 py-1 text-white rounded rounded-3 bg-secondary">Home</Nav.Link>
-            <Nav.Link href="https://www.ghga.de/data/about-ghga-beta" className="mx-2 py-1 text-white">About</Nav.Link>
-            <Nav.Link href="https://www.ghga.de/data/data-download" className="mx-2 py-1 text-white">Download</Nav.Link>
-            <Nav.Link href="https://www.ghga.de/data/data-upload" className="mx-2 py-1 text-white">Upload</Nav.Link>
-            <Nav.Link href="https://www.ghga.de/data/metadata-model" className="mx-2 py-1 text-white">Metadata Model</Nav.Link>
+            <Nav.Link href="/" className="mx-2 p-0"><Button variant="secondary" className="p-0 w-100 h-100 m-0 px-2 text-white">Home</Button></Nav.Link>
+            <Nav.Link href="https://www.ghga.de/data/about-ghga-beta" className="mx-2 p-0"><Button variant="primary" className="p-0 w-100 h-100 m-0 px-2 text-white">About</Button></Nav.Link>
+            <Nav.Link href="https://www.ghga.de/data/data-download" className="mx-2 p-0"><Button variant="primary" className="p-0 w-100 h-100 m-0 px-2 text-white">Download</Button></Nav.Link>
+            <Nav.Link href="https://www.ghga.de/data/data-upload" className="mx-2 p-0"><Button variant="primary" className="p-0 w-100 h-100 m-0 px-2 text-white">Upload</Button></Nav.Link>
+            <Nav.Link href="https://www.ghga.de/data/metadata-model" className="mx-2 p-0"><Button variant="primary" className="p-0 w-100 h-100 m-0 px-2 text-white">Metadata Model</Button></Nav.Link>
           </Nav>
         </Navbar.Collapse>
       </Container>
