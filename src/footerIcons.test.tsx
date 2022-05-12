12fa41aa0443c056a82118cbb4d9a54a4d32378f
alamgirmani <alamgir.mani@gmail.com>
2022-05-12 10:13:33 +0000

    added simple unit test

--- src/footerIcons.test.tsx
+++
@@ -1,8 +0,0 @@
-import { render, screen } from "@testing-library/react"
-import FooterIcons from "./components/footer/footerIcons"
-
-test('on click, open social media icons in new tab', () => {
-    render(<FooterIcons />);
-
-    expect(screen.getByRole('link', { name: /twitter/i })).toBeCalled();
-})
