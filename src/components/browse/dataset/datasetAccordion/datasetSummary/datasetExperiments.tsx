f27e166ca2a710d3cac4e1ddb8a193a4cec04385
alamgirmani <alamgir.mani@gmail.com>
2022-08-08 15:26:28 +0000

    added datasetSummary

--- src/components/browse/dataset/datasetAccordion/datasetSummary/datasetExperiments.tsx
+++ src/components/browse/dataset/datasetAccordion/datasetSummary/datasetExperiments.tsx
@@ -3,32 +3,22 @@ import { Row } from "react-bootstrap";
 import DatasetDetailsLayout from "./datasetDetailsLayout/datasetDetailsLayout";
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { faFlask } from '@fortawesome/free-solid-svg-icons';
-import { experimentEmbeddedModel, hitModel } from "../../../../../models/dataset";
+import { experimentSummaryModel } from "../../../../../models/dataset";
 
 
 interface dataSetExperimentsProps {
-  experimentsList: experimentEmbeddedModel[] | null;
-  hit: hitModel;
+  experiments: experimentSummaryModel | null;
 }
 
 const DatasetExperiments = (props: dataSetExperimentsProps) => {
-  const experiments = props.experimentsList;
-  const protocols: Map<string, number> = new Map<string, number>();
-  if (experiments !== null) {
-    experiments.map((exp) => {
-      if (exp.has_protocol && exp.has_protocol !== null) {
-        exp.has_protocol.map((schema) => {
-          if (schema.instrument_model !== null && schema.instrument_model !== undefined) {
-            var count = protocols.get(schema.instrument_model) !== undefined ? protocols.get(schema.instrument_model)! : 0;
-            protocols.set(schema.instrument_model, count + 1)
-          }
-          return null;
-        });
-      }
-      return null;
-    });
-  }
-
+  const getProtocols = (protocol: { [key: string]: number } | undefined) => {
+    let protocols: string[] = []
+    for (let item in protocol) {
+      let value = protocol[item]
+      protocols.push(item + " : " + value + "\n")
+    }
+    return protocols
+  };
 
   return (
     <DatasetDetailsLayout
@@ -39,25 +29,9 @@ const DatasetExperiments = (props: dataSetExperimentsProps) => {
             <strong>Experiment info</strong>
           </p>
           <br />
-          <p className="mb-0">Experiments: {experiments !== null ? experiments.length : "0"} total</p>
+          <p className="mb-0">Experiments: {props.experiments?.count} total</p>
           <br />
-          <p className="mb-0">Dataset type:&nbsp;{props.hit.content.type}</p>
-          <br />
-          <p className="mb-0">Protocols:&nbsp;</p>
-          <ul className="mb-1 ps-4 ms-3">
-            {experiments !== null
-              ? protocols.size > 0
-                ?
-                Array.from(protocols.entries()).map((item, index) => {
-                  return <div key={index}>
-                    <li key={index}>
-                      <span key={index}> {item[0]} : {item[1]} </span>
-                    </li>
-                  </div>;
-                })
-                : "N/A"
-              : "N/A"}
-          </ul>
+          <p className="mb-0">Protocols: {getProtocols(props.experiments?.stats.protocol)} </p>
         </Row>
       }
     />
--- src/components/browse/dataset/datasetAccordion/datasetSummary/datasetFiles.tsx
+++ src/components/browse/dataset/datasetAccordion/datasetSummary/datasetFiles.tsx
@@ -3,25 +3,22 @@ import { Row } from "react-bootstrap";
 import DatasetDetailsLayout from "./datasetDetailsLayout/datasetDetailsLayout";
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { faFileLines } from '@fortawesome/free-regular-svg-icons';
-import { fileModel } from "../../../../../models/dataset";
+import { fileSummaryModel } from "../../../../../models/dataset";
 import { parseBytes } from "../../../../../utils/utils";
 
 interface dataSetFilesProps {
-  filesList: fileModel[] | null;
+  files: fileSummaryModel | null;
 }
 
 const DatasetFiles = (props: dataSetFilesProps) => {
-  const files = props.filesList;
-  const fileFormats: string[] = [];
-  var totalSize: number = 0;
-  if (files !== null) {
-    files.map((file) => {
-      var fileFormat = fileFormats.find((x) => x === file.format);
-      totalSize += Number(file.size);
-      if (!fileFormat) fileFormats.push(file.format);
-      return null;
-    });
-  }
+  const getFormats = (format: { [key: string]: number } | undefined) => {
+    let formats: string[] = []
+    for (let item in format) {
+      let value = format[item]
+      formats.push(item + " : " + value + "\n")
+    }
+    return formats
+  };
 
   return (
     <DatasetDetailsLayout
@@ -32,27 +29,11 @@ const DatasetFiles = (props: dataSetFilesProps) => {
             <strong>File summary</strong>
             <br />
           </p>
-          {files !== null ? (
+          {props.files !== null ? (
             <div>
-              <p className="mb-0">{files.length} Files</p>
-              <ul className="mb-1 ps-4 ms-3">
-                {fileFormats.map((format) => {
-                  var formatSize: number = 0;
-                  const filteredFiles = files.filter(
-                    (file) => file.format === format
-                  );
-                  filteredFiles.map(
-                    (file) => (formatSize = formatSize + Number(file.size))
-                  );
-                  return (
-                    <li key={filteredFiles.length}>
-                      {filteredFiles.length}&nbsp;{format.toUpperCase()}
-                      &nbsp;files&nbsp;({parseBytes(formatSize)})
-                    </li>
-                  );
-                })}
-              </ul>
-              <p className="mb-0">{parseBytes(totalSize)} total size</p>
+              <p className="mb-0">{props.files.count} Files</p>
+              <p className="mb-0">{parseBytes(props.files.stats.size)} total size</p>
+              <p className="mb-0">File format {getFormats(props.files.stats.format)} </p>
             </div>
           ) : (
             <p className="mb-0">0 Files</p>
