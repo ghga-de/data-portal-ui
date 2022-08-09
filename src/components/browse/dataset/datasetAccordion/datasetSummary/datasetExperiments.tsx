c05a4d7d435863a1926880ca44247d3e4baa1b2b
alamgirmani <alamgir.mani@gmail.com>
2022-08-09 08:36:37 +0000

    removed badge

--- src/components/browse/dataset/datasetAccordion/datasetSummary/datasetExperiments.tsx
+++ src/components/browse/dataset/datasetAccordion/datasetSummary/datasetExperiments.tsx
@@ -1,5 +1,5 @@
 import React from "react";
-import { Badge, Row } from "react-bootstrap";
+import { Row } from "react-bootstrap";
 import DatasetDetailsLayout from "./datasetDetailsLayout/datasetDetailsLayout";
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { faFlask } from '@fortawesome/free-solid-svg-icons';
