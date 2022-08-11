4d2ff26d89734c527bfc54a6307e291feb6157d9
Jordy <31940280+ac-jorellanaf@users.noreply.github.com>
2022-08-11 13:40:47 +0200

    Added back button and request access functionality to SDSV
    - Added back button and request access functionality to SDSV
    - closes GDEV-1111
    - closes GDEV-1097

--- src/components/browse/singleDatasetView/singleDatasetView.tsx
+++ src/components/browse/singleDatasetView/singleDatasetView.tsx
@@ -1,4 +1,4 @@
-import { faKey } from "@fortawesome/free-solid-svg-icons";
+import { faArrowTurnUp, faKey } from "@fortawesome/free-solid-svg-icons";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { useEffect, useState } from "react";
 import { Button, Col, Container, Row, Spinner } from "react-bootstrap";
@@ -8,6 +8,9 @@ import { datasetEmbeddedModel } from "../../../models/dataset";
 import SingleDatasetViewAccordion from "./singleDatasetViewAccordion/singleDatasetViewAccordion";
 import SingleDatasetViewSummary from "./singleDatasetViewSummary/singleDatasetViewSummary";
 import SingleDatasetViewTabs from "./singleDatasetViewTabs/singleDatasetViewTabs";
+import { useNavigate } from "react-router-dom";
+import DataRequestModal from "../dataset/datasetAccordion/datasetSummary/dataRequestModal/dataRequestModal";
+import { getDACEmailId } from "../../../utils/utils";
 
 const SingleDatasetView = (props: any) => {
   const { id } = useParams();
@@ -22,6 +25,14 @@ const SingleDatasetView = (props: any) => {
     datasetEmbeddedModel | null | undefined
   >(null);
 
+  const [show, setShow] = useState(false);
+  const [copyEmail, setCopyEmail] = useState<string>("helpdesk@ghga.de");
+  const handleClose = () => setShow(false);
+
+  var dacFormLink: string | null = null;
+
+  let navigate = useNavigate();
+
   useEffect(() => {
     const getDetails = (datasetId: string) => {
       if (!queried && datasetId) {
@@ -33,14 +44,20 @@ const SingleDatasetView = (props: any) => {
     // eslint-disable-next-line react-hooks/exhaustive-deps
   }, [datasetId]);
 
+  const handleOpen = () => {
+    setCopyEmail(details !== null && details ? getDACEmailId(details) : "helpdesk@ghga.de");
+    setShow(true);
+  };
+
   return (
     <Container className="py-4">
       {details && details !== null ? (
         <>
+          <Button onClick={() => navigate.length <= 2 ? navigate('/browse') : navigate(-1)} variant="white" className="text-secondary mb-3"><FontAwesomeIcon icon={faArrowTurnUp} transform="rotate-270 grow-10 flip-v" /></Button>
           <Button
             className="fs-8 float-end mb-3 ms-4 text-white shadow-md-dark"
             variant="secondary"
-            // onClick={() => handleOpen()}
+            onClick={() => handleOpen()}
             style={{ width: "105px" }}
           >
             <Row className="p-0 m-0 align-items-center">
@@ -52,6 +69,13 @@ const SingleDatasetView = (props: any) => {
               </Col>
             </Row>
           </Button>
+          <DataRequestModal
+              accession={details.accession}
+              copyEmail={copyEmail}
+              show={show}
+              handleClose={handleClose}
+              dacFormLink={dacFormLink}
+            />
           <SingleDatasetViewSummary details={details} />
           <SingleDatasetViewTabs details={details} />
           <SingleDatasetViewAccordion details={details} />
