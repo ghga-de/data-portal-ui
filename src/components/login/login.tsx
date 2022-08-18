30e01000af764af669fc1bdeb1ac7fc049434449
ac-jorellanaf <31940280+ac-jorellanaf@users.noreply.github.com>
2022-08-18 13:36:32 +0000

    Addressing comments

--- src/components/login/login.tsx
+++ src/components/login/login.tsx
@@ -4,28 +4,33 @@ import lsRegister from "../../assets/loginLS/ls-register.png";
 
 const Login = () => {
   return (
-    <Container className="px-5 d-flex justify-content-center">
-      <div className="w-75">
-      <div className="bg-light-alternative p-4 my-5 rounded text-center">
-        <p className="mb-4">
-          To upload/download data to the{" "}
-          <strong>German Human Genome-Phenome Archive</strong>, you need to sign
-          in via a <strong>Life Science</strong> account.
-        </p>
-        <a href="https://profile.aai.lifescience-ri.eu/">
-          <img src={lsLogin} alt="LS Login" width="200px" />
-        </a>
-      </div>
-      <hr className="bg-secondary text-center"/>
-      <div className="bg-light-alternative p-4 my-5 rounded text-center">
-        <p className="mb-4">
-          If you do not have a <strong>Life Science</strong> account, you can
-          register here.
-        </p>
-        <a href="https://signup.aai.lifescience-ri.eu/fed/registrar/?vo=lifescience">
-          <img src={lsRegister} alt="LS Register" width="200px" />
-        </a>
-      </div>
+    <Container className="px-5 d-flex justify-content-center mb-5 pb-5">
+      <div className="w-75 my-5 pb-5">
+        <div className="bg-light-alternative p-4 my-5 rounded text-center mb-5 pb-5">
+          <p className="mb-4">
+            To upload/download data to the{" "}
+            <strong>German Human Genome-Phenome Archive</strong>, you need to
+            sign in via a <strong>Life Science</strong> account.
+          </p>
+          <a href="https://profile.aai.lifescience-ri.eu/">
+            <img src={lsLogin} alt="LS Login" width="200px" />
+          </a>
+        </div>
+        <hr className="bg-secondary text-center" />
+        <div className="bg-light-alternative p-4 my-5 rounded text-center mb-5">
+          <p className="mb-4">
+            If you do not have a <strong>Life Science</strong> account, you can
+            register here.
+          </p>
+          <a href="https://signup.aai.lifescience-ri.eu/">
+            <img
+              src={lsRegister}
+              alt="LS Register"
+              width="200px"
+              className="mb-5"
+            />
+          </a>
+        </div>
       </div>
     </Container>
   );
