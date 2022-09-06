47aefc6c6ca1943e760f39df6627659d5211fe46
Christoph Zwerschke <cito@online.de>
2022-09-06 18:05:16 +0000

    Add authorization header to requests

--- src/api/browse.ts
+++ src/api/browse.ts
@@ -1,5 +1,6 @@
 import { searchResponseModel, datasetEmbeddedModel, datasetSummaryModel } from "../models/dataset";
 import { facetFilterModel } from "../models/facets";
+import authService from "../components/auth/authService";
 
 type getDatasetsSearchRespType = (
   callbackFunc: (hits: searchResponseModel) => void,
@@ -10,13 +11,26 @@ type getDatasetsSearchRespType = (
   documentType: string
 ) => void;
 
-const postHeaders = {
-  Accept: "application/json",
-  "Content-Type": "application/json",
-  Origin: "http://localhost:8000/",
-};
+const getHeaders = async () => {
+  const user = await authService.getUser();
+  const headers: HeadersInit = {
+    "Accept": "application/json",
+    "Content-Type": "application/json"
+  };
+  const origin = process.env.REACT_APP_CLIENT_URL;
+  if (origin) {
+    headers["Origin"] = origin;
+  }
+  const authorization = user?.access_token;
+  if (authorization) {
+    // the Authorization header is already used for Basic auth,
+    // therefore we use the X-Authorization header for the OIDC token
+    headers["X-Authorization"] = authorization;
+  }
+  return headers;
+}
 
-export const querySearchService: getDatasetsSearchRespType = (
+export const querySearchService: getDatasetsSearchRespType = async (
   callbackFunc: (hits: searchResponseModel) => void,
   filterQuery: facetFilterModel[],
   searchKeyword = "*",
@@ -24,29 +38,24 @@ export const querySearchService: getDatasetsSearchRespType = (
   limit: number,
   documentType = "Dataset"
 ) => {
-  fetch(
-    `${process.env.REACT_APP_SVC_SEARCH_URL}/rpc/search?document_type=${documentType}&return_facets=true&skip=${skip}&limit=${limit}`,
-    {
-      method: "POST",
-      headers: postHeaders,
-      body: JSON.stringify({ query: searchKeyword, filters: filterQuery }),
-    }
-  )
-    .then((response) => response.json())
-    .then(
-      (data) => {
-        callbackFunc(data);
-      },
-      (error) => {
-        alert("An error occured while fetching the data.");
-        const errorData: searchResponseModel = {
-          count: -1,
-          hits: [],
-          facets: [],
-        };
-        callbackFunc(errorData);
-      }
-    );
+  let url = `${process.env.REACT_APP_SVC_SEARCH_URL}/rpc/search`;
+  url += `?document_type=${documentType}&return_facets=true&skip=${skip}&limit=${limit}`;
+  const headers = await getHeaders();
+  const body = JSON.stringify({ query: searchKeyword, filters: filterQuery });
+  try {
+    const response = await fetch(url, { method: "POST", headers, body});
+    const data = await response.json();
+    callbackFunc(data);
+  } catch(error) {
+    console.error(error);
+    alert("An error occured while fetching the data.");
+    const errorData: searchResponseModel = {
+      count: -1,
+      hits: [],
+      facets: [],
+    };
+    callbackFunc(errorData);
+  };
 };
 
 type getDatasetDetailsType = (
@@ -55,26 +64,22 @@ type getDatasetDetailsType = (
   callbackFunc: (dataset: datasetEmbeddedModel) => void
 ) => void;
 
-export const getDatasetDetails: getDatasetDetailsType = (
+export const getDatasetDetails: getDatasetDetailsType = async (
   datasetId,
   embedded = false,
   callbackFunc
 ) => {
-  fetch(
-    `${process.env.REACT_APP_SVC_REPOSITORY_URL}/datasets/${datasetId}?embedded=${embedded}`,
-    {
-      method: "get",
-    }
-  )
-    .then((response) => response.json())
-    .then(
-      (data) => {
-        callbackFunc(data);
-      },
-      (error) => {
-        alert("An error occured while fetching the data.");
-      }
-    );
+  let url = `${process.env.REACT_APP_SVC_REPOSITORY_URL}/datasets/${datasetId}`;
+  url += `?embedded=${embedded}`;
+  const headers = await getHeaders();
+  try {
+    const response = await fetch(url, { method: "get", headers});
+    const data = await response.json();
+    callbackFunc(data);
+  } catch(error) {
+    console.error(error);
+    alert("An error occured while fetching the data.");
+  }
 };
 
 type getDatasetSummaryType = (
@@ -82,23 +87,18 @@ type getDatasetSummaryType = (
   callbackFunc: (dataset: datasetSummaryModel) => void
 ) => void;
 
-export const getDatasetSummary: getDatasetSummaryType = (
+export const getDatasetSummary: getDatasetSummaryType = async (
   datasetId,
   callbackFunc
 ) => {
-  fetch(
-    `${process.env.REACT_APP_SVC_REPOSITORY_URL}/dataset_summary/${datasetId}`,
-    {
-      method: "get",
-    }
-  )
-    .then((response) => response.json())
-    .then(
-      (data) => {
-        callbackFunc(data);
-      },
-      (error) => {
-        alert("An error occured while fetching the data.");
-      }
-    );
+  const url = `${process.env.REACT_APP_SVC_REPOSITORY_URL}/dataset_summary/${datasetId}`;
+  const headers = await getHeaders();
+  try {
+    const response = await fetch(url, {method: "get", headers});
+    const data = await response.json()
+    callbackFunc(data);
+  } catch(error) {
+    console.error(error);
+    alert("An error occured while fetching the data.");
+  };
 };
