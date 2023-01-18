2859a318c9b0b4e4ddd33965f5f3000ee48780d3
Christoph Zwerschke <cito@online.de>
2023-01-18 14:07:51 +0000

    Make the registration form nice

--- src/components/register/register.tsx
+++ src/components/register/register.tsx
@@ -1,8 +1,10 @@
 import { ChangeEvent, FormEvent, useEffect, useState } from "react";
-import { Alert, Container, Row, Col } from "react-bootstrap";
+import { Alert, Button, Container, Row, Col } from "react-bootstrap";
 import { useNavigate } from 'react-router-dom';
 import authService, { fullName, User } from "../../services/auth";
 import { fetchJson } from "../../utils/utils";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faUserCheck, faIdCard } from "@fortawesome/free-solid-svg-icons";
 
 const USERS_URL = process.env.REACT_APP_SVC_USERS_URL;
 
@@ -11,6 +13,7 @@ const USERS_URL = process.env.REACT_APP_SVC_USERS_URL;
 const Register = () => {
   const navigate = useNavigate();
   const [user, setUser] = useState<User | null | undefined>(undefined);
+  const [accepted, setAccepted] = useState<boolean>(false);
   let title: string = "";
 
   const postUserData = async () => {
@@ -42,6 +45,10 @@ const Register = () => {
     title = event.target.value;
   };
 
+  const handleToS = (event: ChangeEvent<HTMLInputElement>) => {
+    setAccepted(event.target.checked);
+  };
+
   const handleSubmit = (event: FormEvent) => {
     event.preventDefault();
 
@@ -57,39 +64,53 @@ const Register = () => {
       </div>;
   else
     content = (
-      <div>
-        <h1 style={{margin: "1em 0"}}>Welcome, {fullName(user)}!</h1>
+      <div className="container mb-3">
+        <h1><FontAwesomeIcon icon={faIdCard} className="me-2 text-secondary" /> Registration Form</h1>
+        <h2 className="mt-4">Welcome, {fullName(user)}!</h2>
         <p>Since you haven't used our data portal before,
           we ask you to confirm your user data and register with us.
         </p>
-        <form onSubmit={handleSubmit}>
-          <p>
-            <label>Name:
-            <input type="text" readOnly value={user.name} style={{width:"40em"}}/>
-            </label>
-          </p>
-          <p>
-            <label>E-Mail:
-            <input type="text" readOnly value={user.email} style={{width:"40em"}}/>
-            </label>
-          </p>
-          <p>
-            <label>LifeScience ID:
-            <input type="text" readOnly value={user.lsId} style={{width:"40em"}}/>
-            </label>
-          </p>
-          <p>
-            <label>Academic title:
-            <select onChange={handleTitle}>
-              <option value=""></option>
-              <option value="Dr.">Dr.</option>
-              <option value="Prof.">Prof.</option>
-            </select>
-            </label>
-          </p>
-          <p>
-            <input type="submit" value="Register" />
-          </p>
+        <form onSubmit={handleSubmit} className="mt-4">
+          <div className="row g-3 mb-3">
+            <label className="col-md-2 col-sm-3"><b>Name:</b></label>
+            <div className="col-md-10 col-sm-9">
+              {user.name}
+            </div>
+          </div>
+          <div className="row g-3 mb-3">
+            <label className="col-md-2 col-sm-3"><b>E-Mail:</b></label>
+            <div className="col-md-10 col-sm-9">
+              {user.email}
+            </div>
+          </div>
+          <div className="row g-3 mb-4">
+            <label className="col-md-2 col-sm-3"><b>Life Science ID:</b></label>
+            <div className="col-md-10 col-sm-9">
+              {user.lsId}
+            </div>
+          </div>
+          <div className="row g-3 mb-5">
+            <label className="col-md-2 col-sm-3 col-form-label" htmlFor="input-title"><b>Academic title:</b></label>
+            <div className="col-md-2 col-sm-3">
+              <select onChange={handleTitle} className="form-select" id="input-title">
+                <option value=""></option>
+                <option value="Dr.">Dr.</option>
+                <option value="Prof.">Prof.</option>
+              </select>
+            </div>
+          </div>
+          <div className="row g-3 mb-3">
+            <div className="col-md-12">
+              <input type="checkbox" onChange={handleToS} className="me-3" />I accept
+              the <a href="https://www.ghga.de/terms-of-service" target="_blank" rel="noreferrer">terms of service</a> and
+              the <a href="https://www.ghga.de/data-protection" target="_blank" rel="noreferrer">privacy policy</a>.
+            </div>
+          </div>
+          <div className="d-flex justify-content-end">
+            <Button type="submit" variant="secondary" className="text-white" disabled={!accepted}>
+            <FontAwesomeIcon icon={faUserCheck} className="me-2 ms-3" />
+            <span className="me-3">Register</span></Button>
+          </div>
         </form>
       </div>);
 
