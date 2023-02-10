9a9ec34dd89f978512f8175412e081f3f5013167
Jordy <31940280+ac-jorellanaf@users.noreply.github.com>
2023-02-10 11:05:37 +0100

    New Login popover
    * Added login badge and removed searchbar
    
    * Implemented the newer login Popover
    
    * Added reloading behaviour for logout
    
    * Closes GDEV-1557

--- src/components/header/headerNavbar.tsx
+++ src/components/header/headerNavbar.tsx
@@ -1,12 +1,19 @@
-import React from "react";
-import { useEffect, useState } from "react";
-import logo from "../../assets/GHGA_logo_clean.png";
-import { Navbar, Nav, Button } from "react-bootstrap";
-import { NavLink } from "react-router-dom";
-import HeaderSearchbar from "./headerSearchbar";
-import authService, { User } from "../../services/auth";
+import { faArrowRightToBracket } from "@fortawesome/free-solid-svg-icons";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
-import { faUser } from "@fortawesome/free-solid-svg-icons";
+import { useEffect, useRef, useState } from "react";
+import {
+  Button,
+  Nav,
+  Navbar,
+  Overlay,
+  OverlayTrigger,
+  Popover,
+  Tooltip,
+} from "react-bootstrap";
+import { NavLink } from "react-router-dom";
+import logo from "../../assets/GHGA_logo_clean.png";
+import authService, { User } from "../../services/auth";
+import lsLogin from "../../assets/loginLS/ls-login.png";
 
 const HeaderNavbar = () => {
   const [user, setUser] = useState<User | null>(null);
@@ -27,6 +34,15 @@ const HeaderNavbar = () => {
     "btn btn-secondary p-0 h-100 m-0 mx-2 px-2 pt-1 text-white";
   const inactivePageStyle =
     "btn btn-primary p-0 h-100 m-0 mx-2 px-2 pt-1 text-white";
+
+  const [show, setShow] = useState(false);
+  const target = useRef(null);
+
+  const logout = async () => {
+    await authService.logout();
+    setUser(null);
+    window.location.reload();
+  };
 
   return (
     <Navbar
@@ -68,11 +84,11 @@ const HeaderNavbar = () => {
           aria-controls="basic-navbar-nav"
           className="border-2 text-white"
         />
-        <Navbar.Collapse id="responsive-navbar-nav" className="justify-content-center">
-          <Nav
-            className="justify-content-center"
-            style={{ height: "36px" }}
-          >
+        <Navbar.Collapse
+          id="responsive-navbar-nav"
+          className="justify-content-center"
+        >
+          <Nav className="justify-content-center" style={{ height: "36px" }}>
             <NavLink
               to="/"
               end={true}
@@ -125,30 +141,155 @@ const HeaderNavbar = () => {
           </Nav>
         </Navbar.Collapse>
       </div>
-      <div className="w-25 justify-content-center d-flex">
-        <HeaderSearchbar />
-        <Nav
-            className=""
-            style={{ height: "36px" }}
-          >
-            {user ? (
-              <NavLink
-                to="/profile"
-                className={({ isActive }) =>
-                  isActive ? activePageStyle : inactivePageStyle
+      <div className="w-25 justify-content-end d-flex pe-5">
+        <Nav className="" style={{ height: "36px" }}>
+          {user ? (
+            <>
+              <OverlayTrigger
+                trigger="click"
+                key="profile"
+                placement="bottom-end"
+                overlay={
+                  <Popover
+                    id={"ProfilePopover"}
+                    className="border border-2 border-tertiary"
+                  >
+                    <Popover.Body className="text-center fs-6 px-4">
+                      <p>
+                        <NavLink to="/profile">View profile</NavLink>
+                      </p>
+                      <p className="mt-3">
+                        <a
+                          href="https://profile.aai.lifescience-ri.eu/profile"
+                          target="_blank"
+                          rel="noreferrer"
+                        >
+                          Manage your<br/>LS Login Account
+                        </a>
+                        .
+                      </p>
+                      <Button
+                        variant="secondary"
+                        className="text-white mt-4"
+                        onClick={logout}
+                      >
+                        Logout
+                      </Button>
+                    </Popover.Body>
+                  </Popover>
                 }
               >
-                <FontAwesomeIcon icon={faUser} className="ms-1" /> {user.name}
-              </NavLink>
-            ) : (
-              <NavLink to="/login">
-                <Button variant="secondary" className="text-white"
-                  onClick={onLogin}>
-                  Login <FontAwesomeIcon icon={faUser} className="ms-1" />
-                </Button>
-              </NavLink>
-            )}
-          </Nav>
+                <button
+                  className="bg-tertiary justify-content-center align-items-center d-flex p-0 border-0"
+                  style={{
+                    borderRadius: "50%",
+                    width: "46px",
+                    height: "46px",
+                    marginTop: "-4px",
+                  }}
+                  title="Login"
+                  ref={target}
+                  onMouseEnter={() => setShow(true)}
+                  onMouseLeave={() => setShow(false)}
+                  onMouseDown={() => setShow(!show)}
+                  onClick={onLogin}
+                >
+                  <div
+                    className="bg-quinary justify-content-center align-items-center d-flex text-tertiary"
+                    style={{
+                      borderRadius: "50%",
+                      width: "40px",
+                      height: "40px",
+                    }}
+                  >
+                    {user.name.split(" ").map((x) => x[0])}
+                  </div>
+                </button>
+              </OverlayTrigger>
+              <Overlay target={target.current} show={show} placement="left">
+                {(props) => (
+                  <Tooltip id="overlay-example" {...props}>
+                    You are signed in!
+                  </Tooltip>
+                )}
+              </Overlay>
+            </>
+          ) : (
+            <>
+              <OverlayTrigger
+                trigger="click"
+                key="login"
+                placement="bottom-end"
+                overlay={
+                  <Popover
+                    id={"LoginPopover"}
+                    className="border border-2 border-tertiary"
+                  >
+                    <Popover.Body className="fs-8 text-center">
+                      <p>
+                        You do not need an account to browse our datasets, but
+                        if you wish to request or upload data, you must log in
+                        via Life Science Login (LS Login)
+                      </p>
+                      <p>
+                        LS Login lets you authenticate using your existing
+                        accounts at third parties (your home university,
+                        research institute or a commercial service) and link it
+                        to your LS ID. Alternatively, you can also activate an
+                        LS account with username and password.
+                      </p>
+                      <button className="p-0 border-0">
+                        <img
+                          src={lsLogin}
+                          alt="LS Login"
+                          width="200px"
+                          onClick={() => authService.login()}
+                        />
+                      </button>
+                    </Popover.Body>
+                  </Popover>
+                }
+              >
+                <button
+                  className="bg-tertiary justify-content-center align-items-center d-flex p-0 border-0"
+                  style={{
+                    borderRadius: "50%",
+                    width: "44px",
+                    height: "44px",
+                    marginTop: "-3px",
+                  }}
+                  title="Login"
+                  ref={target}
+                  onMouseEnter={() => setShow(true)}
+                  onMouseLeave={() => setShow(false)}
+                  onMouseDown={() => setShow(!show)}
+                >
+                  <div
+                    className="bg-primary justify-content-center align-items-center d-flex text-tertiary"
+                    style={{
+                      borderRadius: "50%",
+                      width: "40px",
+                      height: "40px",
+                    }}
+                  >
+                    <FontAwesomeIcon
+                      icon={faArrowRightToBracket}
+                      className="m-0"
+                      transform="grow-3"
+                    />
+                  </div>
+                </button>
+              </OverlayTrigger>
+              <Overlay target={target.current} show={show} placement="left">
+                {(props) => (
+                  <Tooltip id="overlay-example" {...props}>
+                    Log In
+                  </Tooltip>
+                )}
+              </Overlay>
+            </>
+          )}
+        </Nav>
       </div>
     </Navbar>
   );
