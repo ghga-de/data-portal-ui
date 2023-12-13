ae029b8e3b720f96187e3face5feaa8430599c46
Seyit Zor <seyitzor@gmail.com>
2023-12-13 10:27:48 +0100

    Update Access Data page (#163)

--- src/components/faq/faq.tsx
+++ src/components/faq/faq.tsx
@@ -108,12 +108,10 @@ const FAQ = () => {
               <sub className="fs-6">
                 Identify your dataset of interest using the browse and filter
                 functions of the GHGA Data Portal. Navigate to the dataset of
-                interest and then click on the "Request access" button. This
-                will prompt your email client to open with an email template to
-                the data access committee or the responsible person for the
-                dataset. Fill in the necessary information and send the email to
-                the data access committee email listed on the dataset. Please
-                note that GHGA is not involved in the process of negotiating the
+                interest and then click on the "Request access" button. This will 
+                direct you to a data access request form. Complete the form with the 
+                necessary information and submit it to request access to the dataset. 
+                Please note that GHGA is not involved in the process of negotiating the
                 data access.
               </sub>
               <li className="fw-bold mt-5">
@@ -127,18 +125,16 @@ const FAQ = () => {
               </sub>
               <li className="fw-bold mt-5">How to get data access?</li>
               <sub className="fs-6">
-                The GHGA Data Portal is currently being developed to allow data
-                download requests over the portal in the near future. During
-                this early phase of the project, we are listing non-personal
-                metadata and acting as a gateway to data submitters who will
-                serve the research data upon approval of the request. Visit your
-                dataset of interest and then click on the "Request access"
-                button. This will prompt your email client to open with an email
-                template to the data access committee for the dataset. Fill in
-                the necessary information and send the email to the data access
-                committee email listed on the dataset. Please note that GHGA is
-                not involved in the further process of negotiating the data
-                access. For more information, please visit the&nbsp;
+                The GHGA Data Portal allows users to request access to data through the 
+                portal. We are listing non-personal metadata and acting as a gateway to 
+                data submitters who will serve the research data upon approval of the
+                request. Visit your dataset of interest and then click on the "Request 
+                access" button. This will direct you to a data access request form. 
+                Complete the form with the necessary information and submit it to 
+                request access to the dataset. The data access committee will review 
+                your request and respond accordingly. Please note that GHGA is not 
+                involved in the further process of negotiating the data access. 
+                For more information, please visit the&nbsp;
                 <Link to="/download">Data Download page</Link>
               </sub>
             </ul>
