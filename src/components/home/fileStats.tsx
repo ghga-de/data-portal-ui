751577890834617106ea1319a6268c4610396281
Christoph Zwerschke <c.zwerschke@dkfz-heidelberg.de>
2024-06-26 14:09:36 +0200

    Aggregate stats for all file types (#209)

---
+++ src/components/home/fileStats.tsx
@@ -0,0 +1,33 @@
+import { MetadataSummaryModel, FileSummaryModel } from "../../models/dataset";
+
+/** Aggregate file stats for all files types. */
+export function aggregateFileStats(
+  summary: MetadataSummaryModel
+): FileSummaryModel {
+  const stats: {
+    [key: string]: FileSummaryModel;
+  } = summary.resource_stats as any;
+  const formats: { [format: string]: number } = {};
+  let count = 0;
+  let size = 0;
+  for (const statsName of Object.keys(stats).filter((key) =>
+    key.endsWith("File")
+  )) {
+    const fileSummary = stats[statsName];
+    count += fileSummary.count || 0;
+    size += fileSummary.stats?.size || 0;
+    for (const format of fileSummary.stats?.format || []) {
+      formats[format.value] = (formats[format.value] || 0) + format.count;
+    }
+  }
+  return {
+    count,
+    stats: {
+      format: Object.entries(formats).map(([value, count]) => ({
+        value,
+        count,
+      })),
+      size,
+    },
+  };
+}
