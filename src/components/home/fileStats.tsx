28bef3e4c02ab336747cdef8201eb159b802b7ff
Jordy <31940280+ac-jorellanaf@users.noreply.github.com>
2024-07-25 11:38:50 +0200

    Fixed error with fileStats in aggregation (#226)

--- src/components/home/fileStats.tsx
+++ src/components/home/fileStats.tsx
@@ -4,30 +4,34 @@ import { MetadataSummaryModel, FileSummaryModel } from "../../models/dataset";
 export function aggregateFileStats(
   summary: MetadataSummaryModel
 ): FileSummaryModel {
-  const stats: {
-    [key: string]: FileSummaryModel;
-  } = summary.resource_stats as any;
-  const formats: { [format: string]: number } = {};
-  let count = 0;
-  let size = 0;
-  for (const statsName of Object.keys(stats).filter((key) =>
-    key.endsWith("File")
-  )) {
-    const fileSummary = stats[statsName];
-    count += fileSummary.count || 0;
-    size += fileSummary.stats?.size || 0;
-    for (const format of fileSummary.stats?.format || []) {
-      formats[format.value] = (formats[format.value] || 0) + format.count;
+  if (summary.resource_stats) {
+    const stats: {
+      [key: string]: FileSummaryModel;
+    } = summary.resource_stats as any;
+    const formats: { [format: string]: number } = {};
+    let count = 0;
+    let size = 0;
+    for (const statsName of Object.keys(stats).filter((key) =>
+      key.endsWith("File")
+    )) {
+      const fileSummary = stats[statsName];
+      count += fileSummary.count || 0;
+      size += fileSummary.stats?.size || 0;
+      for (const format of fileSummary.stats?.format || []) {
+        formats[format.value] = (formats[format.value] || 0) + format.count;
+      }
     }
+    return {
+      count,
+      stats: {
+        format: Object.entries(formats).map(([value, count]) => ({
+          value,
+          count,
+        })),
+        size,
+      },
+    };
+  } else {
+    return { count: 0, stats: { format: [], size: 0 } };
   }
-  return {
-    count,
-    stats: {
-      format: Object.entries(formats).map(([value, count]) => ({
-        value,
-        count,
-      })),
-      size,
-    },
-  };
 }
