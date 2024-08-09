83359ce99e2cde989e4829fd41de58610cf4cce0
Christoph Zwerschke <c.zwerschke@dkfz-heidelberg.de>
2024-08-09 10:39:53 +0000

    Simplify and update top section on home page

--- src/components/home/homeTopSection/homeTopSection.tsx
+++ src/components/home/homeTopSection/homeTopSection.tsx
@@ -1,6 +1,5 @@
 import { Button, Col, Row } from "react-bootstrap";
-import germany from "../../../assets/homepage/Germany.svg";
-import { Link, NavLink } from "react-router-dom";
+import { NavLink } from "react-router-dom";
 
 /** Section on top of the home page where general information is given about platform. */
 const HomeTopSection = () => {
@@ -8,43 +7,22 @@ const HomeTopSection = () => {
   const LINK_CLASS = "text-secondary";
   return (
     <Col className="px-lg-2">
-      <Row className="w-100 mx-0 mb-3 pb-4 pe-lg-4">
-        <Col xs={12} sm={"auto"} className="px-lg-5 text-center text-lg-start">
-          <img src={germany} alt="Germany" height="400px" className="mw-100" />
-        </Col>
-        <Col className="p-2 p-lg-5">
+      <Row className="w-100 mx-0 mb-2 pb-4 pe-lg-4">
+        <Col className="p-2 p-lg-4">
           <p className={PARAGRAPH_CLASS}>
-            The GHGA Data Portal is a discovery platform for human omics data
-            available for research that can be requested from one of the GHGA
-            Data Hubs.
+            The GHGA Data Portal is a secure national infrastructure for human omics
+            data available under controlled access. Access to the archived data can
+            be requested from the data controllers who are responsible for evaluating
+            your data access request.
           </p>
           <p className={PARAGRAPH_CLASS}>
-            The public catalog is a first step towards our goal to provide
-            comprehensive data archival services for human omics data.
-          </p>
-          <p className={PARAGRAPH_CLASS}>
-            The datasets within are annotated following the GHGA Metadata Model,
+            The datasets within are annotated following the{" "}
+            <a className={LINK_CLASS} href="https://docs.ghga.de/metadata/overview/">GHGA Metadata Model</a>,
             which is compatible with the metadata model of the EGA.
           </p>
           <p className={PARAGRAPH_CLASS}>
-            Please also see further documentation under&nbsp;
-            <Link
-              to="/download"
-              target="_blank"
-              rel="noreferrer"
-              className={LINK_CLASS}
-            >
-              “How to get data access”
-            </Link>
-            , and&nbsp;
-            <Link
-              to="/upload"
-              target="_blank"
-              rel="noreferrer"
-              className={LINK_CLASS}
-            >
-              “Submit data to GHGA”
-            </Link>
+            For further documentation, please visit the{" "}
+            <a className={LINK_CLASS} href="https://docs.ghga.de/">GHGA User Documentation</a>.
           </p>
           <div className="text-center">
             <NavLink to="/browse">
@@ -52,7 +30,7 @@ const HomeTopSection = () => {
                 variant="quinary"
                 className="py-1 fs-5 mt-4 text-white shadow-md-dark"
               >
-                <span className="text-uppercase">Get started</span>
+                <span className="text-uppercase">Browse data</span>
               </Button>
             </NavLink>
           </div>
