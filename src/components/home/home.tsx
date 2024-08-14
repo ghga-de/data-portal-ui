3c4ae0cf41af860b511ac253a53dc73d4d096eab
ac-jorellanaf <31940280+ac-jorellanaf@users.noreply.github.com>
2024-08-14 16:43:08 +0000

    Some more updates to headings

--- src/components/home/home.tsx
+++ src/components/home/home.tsx
@@ -9,23 +9,23 @@ const Home = () => {
     <Row className="px-2 pt-4 pt-lg-0 mx-0 w-100">
       <Col className="mx-0 p-0 w-100">
         <h2 className="fw-bold p-lg-3 pb-2 pb-lg-2 mb-0 text-center">
-          The German Human Genome-Phenome Archive
-        </h2>
-        <h2 className="fw-bold pb-2 px-4 text-quaternary text-center">
-          Data Portal
+          <span className="fs-3 mb-1 d-block">
+            The German Human Genome-Phenome Archive
+          </span>
+          <span className="text-quaternary">Data Portal</span>
         </h2>
         <Row className="m-0 w-100">
           <div>
             <hr className="mx-lg-3 border-tertiary opacity-100" />
           </div>
         </Row>
-        <Row className="mx-0 w-100">
+        <Row className="mx-0 w-100" as="section">
           <HomeTopSection />
         </Row>
-        <Row className="mx-0 w-100">
+        <Row className="mx-0 w-100" as="section">
           <HomeMidSection />
         </Row>
-        <Row className="mx-0 w-100">
+        <Row className="mx-0 w-100" as="aside">
           <HomeBottomSection />
         </Row>
       </Col>
